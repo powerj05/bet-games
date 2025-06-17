47ccf8502c855773646f04242fba9f9f47d1c316
John Power <25281962+powerj05@users.noreply.github.com>
2025-06-17 13:08:00 +0100

    Updated code to successfully identify the top 500 golfers and store them in a list.

--- webscrape_test.py
+++ webscrape_test.py
@@ -9,15 +9,34 @@ import requests
 url = 'https://www.owgr.com/current-world-ranking'
 driver = webdriver.Firefox()
 driver.get(url)
+golfers = []
+wait = WebDriverWait(driver,5)
 
-wait = WebDriverWait(driver,10)
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, "table__link-column")))
+wait.until(EC.presence_of_element_located((By.XPATH,"//button[contains(text(), 'Accept All Cookies')]")))
+cookies = driver.find_element(By.XPATH,"//button[contains(text(), 'Accept All Cookies')]")
+cookies.click()
 
+wait.until(EC.presence_of_element_located((By.CLASS_NAME,"table__link-column")))
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 divs = soup.find_all('div', class_ = 'table__link-column')
 
-golfers = []
-
-for i in range(0,500):
+for i in range(0,100):
     print(divs[i].text)
     golfers.append(divs[i].text)
+
+
+for i in range(1,5):
+    # click "next page" button
+    xpath = "//a[@aria-label='Page {npage_no}']".format(npage_no = i+1)
+    next_page = wait.until(EC.element_to_be_clickable((By.XPATH,xpath)))
+    driver.execute_script("arguments[0].click();", next_page)
+
+    wait.until(EC.presence_of_element_located((By.CLASS_NAME, "table__link-column")))
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    divs = soup.find_all('div', class_ = 'table__link-column')
+
+    for i in range(0,100):
+        # repeat above code to add next 100 golfers
+        print(divs[i].text)
+        golfers.append(divs[i].text)
